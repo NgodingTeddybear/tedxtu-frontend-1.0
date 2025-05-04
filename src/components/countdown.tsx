9599bd37114b48bfdf2aa42d7871713076e35820
ScaferuZ <endrafruz@gmail.com>
2025-05-04 13:28:06 +0700

    feat: change countdown

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -3,15 +3,9 @@
 import useCountdown from '@/hooks/useCountdown';
 
 export default function Countdown() {
-    const timeLeft = useCountdown(new Date('2025-05-04T12:00:00+07:00'));
-
     return (
         <p className='font-bold text-xl'>
-            We'll start in :{' '}
-            <span className='text-[#FFB41E]'>
-                {timeLeft.days}d {timeLeft.hours}h {timeLeft.minutes}m{' '}
-                {timeLeft.seconds}s
-            </span>
+            <span className='text-[#FFB41E] font-raleway'>Our event has started! Come to the venue for OTS</span>
         </p>
     );
 }
